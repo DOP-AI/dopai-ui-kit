7ddcffef73433941c93a608fcac48e4c059c7a8f
tcanant84 <53287562+tcanant84@users.noreply.github.com>
2026-05-08 00:06:07 -0700

    Create typography.ts

---
+++ src/tokens/typography.ts
@@ -0,0 +1,31 @@
+export const typography = {
+  fontFamily: {
+    primary: `"Inter", sans-serif`,
+    mono: `"IBM Plex Mono", monospace`,
+  },
+
+  fontSize: {
+    xs: "12px",
+    sm: "14px",
+    base: "16px",
+    lg: "20px",
+    xl: "24px",
+    "2xl": "32px",
+    "3xl": "40px",
+  },
+
+  fontWeight: {
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+  },
+
+  lineHeight: {
+    tight: 1.1,
+    normal: 1.5,
+    relaxed: 1.75,
+  },
+} as const;
+
+export type TypographyTokens = typeof typography;
